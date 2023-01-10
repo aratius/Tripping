f0a96e5136970a7e586d1531b67d3c3737b50643
aratius <arata1129matsu@icloud.com>
2023-01-10 19:37:51 +0900

    multiple balls

--- equation-of-motion/equation-of-motion.ino
+++ equation-of-motion/equation-of-motion.ino
@@ -4,17 +4,22 @@
 #endif
 
 #define PIN        3 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 32 // Popular NeoPixel ring size
+#define NUMPIXELS 64 // Popular NeoPixel ring size
+#define BALL_NUM 3
+
+struct Ball {
+  float v;
+  float pos;  
+};
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-float body_size = 4;  // 体の大きさ
+float body_size = 3;  // 体の大きさ
 float g = 0;  // 重力加速度
 float k = 1000;  // バネ定数
-float c = 5;
-float v = 0;
+float c = 10;
+struct Ball balls[BALL_NUM] = {{0, 0}, {0, 0}, {0, 0}};
 float time = 0;  // 時間
 float last_time = 0;  // 前回の時間
-float pos = 0;  // 現在の座標
 float floor_pos = 0;  // 床の位置
 float ceil_pos = NUMPIXELS;  // 天井の位置
 int bound_cnt = 0;  // バウンド回数
@@ -43,13 +48,14 @@ void loop() {
 
   float seq_time = 15;  // シーケンス全体の時間
   // %使いたいがためにint化
-  if(int(time * 1000) % int(seq_time * 1000) < int(5 * 1000)) {
+  if(long(time * 1000) % long(seq_time * 1000) < long(5 * 1000)) {
     g = -9.8 * 30;
-  } else if(int(time * 1000) % int(seq_time * 1000) < int(5.2 * 1000)) {
+  } else if(long(time * 1000) % long(seq_time * 1000) < long(5.2 * 1000)) {
     g = 9.8 * 30;
-  } else if(int(time * 1000) % int(seq_time * 1000) < int(5.4 * 1000)) {
+  } else if(long(time * 1000) % long(seq_time * 1000) < long(5.4 * 1000)) {
     g = -9.8 * 20;
   } else {
+    float pos = balls[0].pos;
     float dist_from_center_nomralized = (pos - (ceil_pos / 2)) / (ceil_pos / 2);
     g = 9.8 * 6 * -dist_from_center_nomralized;
     g += (sin(time * .9) + cos(time * .95)) * 9.8 * 2;  // 収束しないようにsinで適当に揺らし続ける
@@ -59,17 +65,21 @@ void loop() {
 
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
     float color[3] = {0, 0, 0};
-    float dist = abs(pos - (float)i);
-    
-    float fade_max = body_size/2;
-    float fade_min = body_size/2 + 2;
-    float a_max = 255;
-    float a_min = 0;
-    float a = ((a_max - a_min) / (fade_max - fade_min)) * dist + (fade_max * a_min - fade_min * a_max) / (fade_max - fade_min);
-    a = max(min(a, a_max), a_min);
-    color[0] = a;
-    color[1] = a;
-    color[2] = a;
+    for(int j = 0; j < BALL_NUM; j++) {
+      float pos = balls[j].pos;
+      float dist = abs(pos - (float)i);
+
+      float fade_max = body_size/2;
+      float fade_min = body_size/2 + 2;
+      float a_max = 255;
+      float a_min = 0;
+      float a = ((a_max - a_min) / (fade_max - fade_min)) * dist + (fade_max * a_min - fade_min * a_max) / (fade_max - fade_min);
+      a = max(min(a, a_max), a_min);
+      color[j % 3] += a;
+    }
+    color[0] = max(min(color[0], 255), 0);
+    color[1] = max(min(color[1], 255), 0);
+    color[2] = max(min(color[2], 255), 0);
     if(color[0] > 0 || color[1] > 0 || color[2] > 0) pixels.setPixelColor(i, pixels.Color(color[0], color[1], color[2]));
 
   }
@@ -79,34 +89,54 @@ void loop() {
 }
 
 void initSimulation() {
-  pos = (float)NUMPIXELS - body_size - 1;
+  for(int i = 0; i < BALL_NUM; i++) {
+    balls[i].pos = (float)NUMPIXELS - body_size * (float)(i + 1) * 3 - 1;
+  }
   floor_pos = 0;
   g = -9.8 * 30;
 }
 
 void simulate() {
   float delta_time = time - last_time;
-  float f = g;
-  float r = body_size/2;
+  for(int i = 0; i < BALL_NUM; i++) {
+    Ball me = balls[i];
+    float f = g;
+    float r = body_size/2;
+    float v = me.v;
+    float pos = me.pos;
 
-  // 地面や天井に沈み込んだときの演算
-  if (pos < r + floor_pos) {
-    f += k * (r - (pos - floor_pos)) - c * v;
-  } else if (pos > - r + ceil_pos) {
-    f += k * (- r - (pos - ceil_pos)) - c * v;
+    // 地面や天井に沈み込んだときの演算
+    if (pos < r + floor_pos) {
+      f += k * (r - (pos - floor_pos)) - c * v;
+    } else if (pos > - r + ceil_pos) {
+      f += k * (- r - (pos - ceil_pos)) - c * v;
+    }
+
+    for(int j = 0; j < BALL_NUM; j++) {
+      if(i == j) continue;
+      Ball other = balls[j];
+      float dist = abs(me.pos - other.pos);
+      if(dist < r * 2) {
+        float pow_dir = me.pos > other.pos ? 1 : -1;
+        f += k * (r * 2 - dist) * pow_dir - c * v * pow_dir;
+      }
+    }
+
+    float a = equation_of_motion(1, f);
+    v += a * delta_time;
+    v *= .99;  // 空気抵抗
+    balls[i].v = v;
   }
-
-  float a = equation_of_motion(1, f);
-  v += a * delta_time;
-  v *= .99;  // 空気抵抗
-  pos += v * delta_time;
+  for(int i = 0; i < BALL_NUM; i++) {
+    balls[i].pos += balls[i].v * delta_time;
+  }
 }
 
 void reverse() {
   g *= -1;
 }
  
-float sign(float v) {
-  if(v < 0) return -1;
+float sign(float value) {
+  if(value < 0) return -1;
   else return 1;
 }
