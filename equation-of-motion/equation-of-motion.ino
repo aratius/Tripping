e4480738c84541ebcd95cbb6f86063bd5bc2d468
aratius <arata1129matsu@icloud.com>
2023-01-10 17:51:12 +0900

    equation of motion

--- equation-of-motion/equation-of-motion.ino
+++ equation-of-motion/equation-of-motion.ino
@@ -4,28 +4,31 @@
 #endif
 
 #define PIN        3 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 64 // Popular NeoPixel ring size
+#define NUMPIXELS 32 // Popular NeoPixel ring size
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-float body_size = 30;  // 体の大きさ
-float g = -9.8 * 1;  // 重力加速度
-float v0 = 0;  // 初速
+float body_size = 4;  // 体の大きさ
+float g = 0;  // 重力加速度
+float k = 1000;  // バネ定数
+float c = 5;
+float v = 0;
 float time = 0;  // 時間
 float last_time = 0;  // 前回の時間
-float simulation_time_started = 0;  // 演算のための
 float pos = 0;  // 現在の座標
 float floor_pos = 0;  // 床の位置
 float ceil_pos = NUMPIXELS;  // 天井の位置
 int bound_cnt = 0;  // バウンド回数
 
-float free_fall(float _v0, float _g, float _t) {
-  return _v0 + _g * _t;
+float equation_of_motion(float m, float f){
+  return f / m;
 }
 
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
+
+  Serial.begin(9600);
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
@@ -36,7 +39,7 @@ void setup() {
 void loop() {
   pixels.clear(); // Set all pixel colors to 'off'
 
-  time = millis();
+  time = (float)millis() / 1000.;
   simulate();
 
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
@@ -51,28 +54,25 @@ void loop() {
 }
 
 void initSimulation() {
-  simulation_time_started = time;
-  pos = (float)NUMPIXELS;
+  pos = (float)NUMPIXELS - body_size - 1;
   floor_pos = 0;
-  g = -9.8 * 1;
+  g = -9.8 * 10;
 }
 
 void simulate() {
-  float v = free_fall(v0, g, (time - simulation_time_started) / 1000);
-  pos += v;
-  if(
-    (v <= 0 && pos - body_size / 2 < floor_pos) ||
-    (v > 0 && pos + body_size / 2 > ceil_pos)
-  ) bound(v);
-}
+  float delta_time = time - last_time;
+  float f = g;
+  float r = body_size/2;
 
-void bound(float v) {
-  v0 = -v * .8;
-  simulation_time_started = time;
-  pos = v < 0 ? floor_pos + body_size / 2 : ceil_pos - body_size / 2;
-  bound_cnt++;
+  if (pos < r + floor_pos) {
+    f += k * (r - (pos - floor_pos)) - c * v;
+  } else if (pos > - r + ceil_pos) {
+    f += k * (- r - (pos - ceil_pos)) - c * v;
+  }
 
-  if(bound_cnt % 20 == 0) reverse();
+  float a = equation_of_motion(1, f);
+  v += a * delta_time;
+  pos += v * delta_time;
 }
 
 void reverse() {
